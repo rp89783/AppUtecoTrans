a6bef60ee9eeab033e70e401d727ef5178834bb7
rp89783 <rp89783@gmail.com>
2019-10-27 06:44:39 -0400

    v1

---
+++ AppUtecoTrans/AppUtecoTrans/PgLogin.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppUtecoTrans
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PgLogin : ContentPage
+    {
+        public PgLogin()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRegistar_Clicked(object sender, EventArgs e)
+        {
+           
+            await Navigation.PushModalAsync(new PgRegistrar());
+            
+        }
+
+        private async void btnAcceder_Clicked(object sender, EventArgs e)
+        {
+            
+           
+        }
+    }
+}
---
+++ AppUtecoTrans/AppUtecoTrans/PgRegistrar.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppUtecoTrans
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PgRegistrar : ContentPage
+    {
+        public PgRegistrar()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+           
+            
+        }
+    }
+}
